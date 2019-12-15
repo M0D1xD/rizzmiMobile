65fa0b211497d7682337a95d09d94c165ecb1ef7
Mohammad Zakout <modi.zak.5@gmail.com>
2019-12-15 16:38:42 +0400

    Authentication implemented

--- app/src/main/java/com/example/rizzmi/ui/auth/SignInFragment.java
+++ app/src/main/java/com/example/rizzmi/ui/auth/SignInFragment.java
@@ -1,7 +1,6 @@
 package com.example.rizzmi.ui.auth;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,18 +8,33 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.rizzmi.MainActivity;
 import com.example.rizzmi.R;
+import com.example.rizzmi.model.api.RegResponse;
+import com.example.rizzmi.model.api.Credentials;
+import com.example.rizzmi.service.ApiServices;
+import com.example.rizzmi.service.RetrofitInstance;
+import com.google.android.material.textfield.TextInputEditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignInFragment extends Fragment {
+
+    private static final String TAG = "SignInFragment";
+    private Call<RegResponse> loginCall;
+    private TextInputEditText et_email;
+    private TextInputEditText et_password;
 
 
     public SignInFragment() {
@@ -39,11 +53,41 @@ public class SignInFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        ApiServices mService = RetrofitInstance.getService();
+        initViews(view);
+
         view.findViewById(R.id.tv_sign_up).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_sign_up));
         view.findViewById(R.id.btn_login).setOnClickListener(v -> {
-            Intent intent = new Intent(getContext(), MainActivity.class);
-            startActivity(intent);
-            getActivity().finish();
+
+
+            if (TextUtils.isEmpty(et_email.getText()))
+                return;
+
+            if (TextUtils.isEmpty(et_password.getText()))
+                return;
+
+            Credentials credentials = new Credentials(et_email.getText().toString(), et_password.getText().toString());
+
+
+            loginCall = mService.login(credentials);
+            loginCall.enqueue(new Callback<RegResponse>() {
+                @Override
+                public void onResponse(Call<RegResponse> call, Response<RegResponse> response) {
+                    Log.d(TAG, "onResponse: " + response.body().toString());
+                }
+
+                @Override
+                public void onFailure(Call<RegResponse> call, Throwable t) {
+                    Log.d(TAG, "onFailure: " + t.getMessage());
+                }
+            });
+
         });
+    }
+
+    private void initViews(View view) {
+        et_email = view.findViewById(R.id.et_email);
+        et_password = view.findViewById(R.id.et_password);
+
     }
 }
--- app/src/main/java/com/example/rizzmi/ui/auth/SignUpFragment.java
+++ app/src/main/java/com/example/rizzmi/ui/auth/SignUpFragment.java
@@ -3,22 +3,45 @@ package com.example.rizzmi.ui.auth;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatCheckBox;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.rizzmi.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignUpFragment extends Fragment {
+    private Unbinder unbinder;
+
+    @BindView(R.id.et_first_name)
+    TextInputEditText et_first;
+    @BindView(R.id.et_last_name)
+    TextInputEditText et_last;
+    @BindView(R.id.et_password)
+    TextInputEditText et_password;
+    @BindView(R.id.et_phone)
+    TextInputEditText et_phone;
+    @BindView(R.id.cb_agree)
+    AppCompatCheckBox cb_agree;
+    @BindView(R.id.tv_sign_in)
+    TextView tv_sign_in;
+    @BindView(R.id.btn_sign_up)
+    MaterialButton btn_sign_up;
 
 
     public SignUpFragment() {
@@ -30,13 +53,24 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+
+        btn_sign_up.setOnClickListener(v -> {
+
+        });
+        return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.tv_sign_in).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_sign_in));
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 
+    @OnClick(R.id.tv_sign_in)
+    void SignIn(View view) {
+        view.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_sign_in));
     }
 }
